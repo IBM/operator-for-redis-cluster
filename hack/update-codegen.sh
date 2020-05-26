b02218c10fe1315252258e82316f75bfb0cd1402
Tomash Sidei <tomash.sidei@ibm.com>
2020-05-26 19:57:12 +0300

    Update the code generation script/

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -4,4 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-./vendor/k8s.io/code-generator/generate-groups.sh all github.com/amadeusitgroup/redis-operator/pkg/client github.com/amadeusitgroup/redis-operator/pkg/api redis:v1 --go-header-file ./hack/custom-boilerplate.go.txt
+#	`go get sigs.k8s.io/controller-tools/cmd/controller-gen@v0.2.5` to install the tool
+controller-gen object:headerFile=./hack/custom-boilerplate.go.txt paths="github.com/amadeusitgrgoup/redis-operator/pkg/..."
+
+# old code generator command
+#./vendor/k8s.io/code-generator/generate-groups.sh all github.com/amadeusitgroup/redis-operator/pkg/client github.com/amadeusitgroup/redis-operator/pkg/api redis:v1 --go-header-file ./hack/custom-boilerplate.go.txt
