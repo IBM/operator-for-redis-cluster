bba1dbccdec427dc7d41d3b89faa334d956f2f5f
Tymofii Polekhin <tpolekhin@users.noreply.github.com>
2021-05-24 10:54:51 +0300

    Update build info variables. Pass docker build args. (#64)
    * Update build info variables. Pass docker build args.
    
    * Extract image labels into an output JSON map
    
    * Use added meta json output

--- pkg/utils/build.go
+++ pkg/utils/build.go
@@ -5,22 +5,14 @@ import (
 	"time"
 )
 
-//BUILDTIME should be populated by at build time: -ldflags "-w -X github.com/TheWeatherCompany/icm-redis-operator/pkg/utils.BUILDTIME=${DATE}
-//with for example DATE=$(shell date +%Y-%m-%d/%H:%M:%S )   (pay attention not to use space!)
 var BUILDTIME string
-
-//TAG should be populated by at build time: -ldflags "-w -X github.com/TheWeatherCompany/icm-redis-operator/pkg/utils.TAG=${TAG}
-//with for example TAG=$(shell git tag|tail -1)
-var TAG string
-
-//COMMIT should be populated by at build time: -ldflags "-w -X github.com/TheWeatherCompany/icm-redis-operator/pkg/utils.COMMIT=${COMMIT}
-//with for example COMMIT=$(shell git rev-parse HEAD)
-var COMMIT string
+var VERSION string
+var REVISION string
 
 // BuildInfos returns builds information
 func BuildInfos() {
 	fmt.Println("Program started at: " + time.Now().String())
 	fmt.Println("BUILDTIME=" + BUILDTIME)
-	fmt.Println("TAG=" + TAG)
-	fmt.Println("COMMIT=" + COMMIT)
+	fmt.Println("VERSION=" + VERSION)
+	fmt.Println("REVISION=" + REVISION)
 }
